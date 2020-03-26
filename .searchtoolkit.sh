4c2865c46be5f688b2edb895e1919c3b302e203d
Connor Olsen <connorolsen72@gmail.com>
2020-03-26 10:30:24 -0600

    adds my searchin function :)

--- .searchtoolkit.sh
+++ .searchtoolkit.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
+function searchin(){
+  reset;
+  echo -e "\033[1;34mSearchin...\033[0m"
+  if [[ $1 == "" ]] || [[ $1 == "-h" ]]
+  then
+    printf "Searchin recursivly searches all files below the currenty directory for one or \ntwo keywords using grep. Error messages are not displayed, and results are \nprinted out in a clean and colored forman\n
+    -a:
+      Display the five lines around the search result line\n
+    -h:
+      Show this menu"
+  fi
+  if [[ $1 == "-a" ]]
+  then
+    if[ "$#" -eq 3 ]
+    then
+      grep --context=5 -rniI --color --exclude='*.log' -E $2.*$3\|$3.*$2 2>/dev/null
+    else
+      grep --context=5 -rniI --color --exclude='*.log' -E $2 2>/dev/null
+    fi
+  else
+    if [ "$#" -eq 2 ]
+    then
+       grep -rniI --color --exclude='*.log' -E $1.*$2\|$2.*$1 2>/dev/null
+    else
+      grep -rniI --color --exclude='*.log' -E $1 2>/dev/null
+    fi 
 #insert my searchin function here somewhere
